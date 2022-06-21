31983cdb816b972d2e39745aac197b5664d60d3e
Derek Ellis <derekellis170@gmail.com>
2022-06-21 11:10:56 -0400

    Add jar manifest config
    Update deploy

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
   alias(libs.plugins.kotlin.jvm)
   alias(libs.plugins.ksp)
   alias(libs.plugins.shadow)
+  application
 }
 
 group = "ca.derekellis.api"
@@ -32,4 +33,8 @@ kotlin {
 
 tasks.withType<KotlinCompile> {
   kotlinOptions.freeCompilerArgs += "-Xcontext-receivers"
+}
+
+application {
+  mainClass.set("ca.derekellis.api.server.MainKt")
 }
