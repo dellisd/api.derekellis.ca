dc66df3af566d574d0f997851c51cfeeb931d089
Derek Ellis <derekellis170@gmail.com>
2022-06-21 22:48:16 -0400

    Decode raw lambda input

--- lambda/src/main/kotlin/ca/derekellis/api/lambda/RaptorHandler.kt
+++ lambda/src/main/kotlin/ca/derekellis/api/lambda/RaptorHandler.kt
@@ -7,9 +7,12 @@ import com.amazonaws.services.lambda.runtime.RequestStreamHandler
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
 import kotlinx.coroutines.runBlocking
+import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonObject
 import kotlinx.serialization.json.decodeFromStream
 import kotlinx.serialization.json.encodeToStream
+import kotlinx.serialization.json.jsonPrimitive
 import org.slf4j.LoggerFactory
 import java.io.InputStream
 import java.io.OutputStream
@@ -18,7 +21,9 @@ class RaptorHandler : RequestStreamHandler {
   private val logger = LoggerFactory.getLogger(javaClass)
 
   override fun handleRequest(input: InputStream, output: OutputStream, context: Context) {
-    val request: RaptorRequest = Json.decodeFromStream(input)
+    val rawInput = Json.decodeFromStream<JsonObject>(input)
+
+    val request: RaptorRequest = Json.decodeFromString(rawInput.getValue("body").jsonPrimitive.content)
     logger.info("Received: $request")
 
     val result = runBlocking(Dispatchers.Default) {
